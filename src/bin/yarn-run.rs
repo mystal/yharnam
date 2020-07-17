8abba8c2f46b676549cf7a515edd75b8f3f9c37c
Gabriel Martinez <reitaka@gmail.com>
2020-07-17 16:36:05 -0700

    Load the csv file and prep to write runtime

--- src/bin/yarn-run.rs
+++ src/bin/yarn-run.rs
@@ -2,22 +2,49 @@ use std::env;
 use std::error::Error;
 use std::fs;
 use std::io::Cursor;
+use std::path::PathBuf;
 
 use prost::Message;
+use serde::Deserialize;
 
 use yarn_runner::yarn;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    // Read first argument, try to open the file there. Try to load it as a yarnc protobuf.
-    let f = env::args().nth(1)
-        .unwrap();
-    dbg!(&f);
+#[derive(Debug, Deserialize)]
+pub struct Record {
+    id: String,
+    text: String,
+    file: String,
+    node: String,
+    #[serde(rename="lineNumber")]
+    line_number: u32,
+}
 
-    let data = fs::read(&f)?;
-    let program = yarn::Program::decode(&mut Cursor::new(&data))?;
-    dbg!(&program);
+fn main() -> Result<(), Box<dyn Error>> {
+    // Read first argument as a path to a yarnc file.
+    let proto_path = env::args().nth(1)
+        .unwrap();
+    let proto_path = PathBuf::from(proto_path);
+
+    // Read the file's bytes and load a Program.
+    let proto_data = fs::read(&proto_path)?;
+    let program = yarn::Program::decode(&mut Cursor::new(&proto_data))?;
+    println!("{:#?}", &program);
+
+    // Load Records from a csv file.
+    let mut csv_path = proto_path;
+    csv_path.set_extension("csv");
+    let mut csv_reader = csv::Reader::from_path(csv_path)?;
+    let records: Vec<Record> = csv_reader.deserialize()
+        .map(|result| result.unwrap())
+        .collect();
+    // dbg!(records);
 
     // TODO: Run the virtual machine!
+    // TODO: Find the Start node.
+    if let Some(node) = program.nodes.get("Start") {
+    } else {
+        eprintln!("Could not find Start node");
+    }
 
     Ok(())
 }
